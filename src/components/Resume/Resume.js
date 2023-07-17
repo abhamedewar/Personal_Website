433be668970912edbf302c2f14d4f87313295fe4
Abhishri <abhamedewar@gmail.com>
2023-07-16 20:15:59 -0700

    updated file

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -8,7 +8,7 @@ const Resume = () => {
   const handleView = () => {
     // Create a temporary <a> element to trigger the download
     const link = document.createElement("a");
-    link.href = process.env.PUBLIC_URL + "/file.pdf";
+    link.href = process.env.PUBLIC_URL + "/Abhishri_Ajit_Medewar.pdf";
     link.target = "_blank";
     link.rel = "noopener noreferrer";
     link.type = "application/pdf";
@@ -18,7 +18,7 @@ const Resume = () => {
   const handleDownload = () => {
     // Create a temporary <a> element to trigger the download
     const link = document.createElement("a");
-    link.href = process.env.PUBLIC_URL + "/file.pdf";
+    link.href = process.env.PUBLIC_URL + "/Abhishri_Ajit_Medewar.pdf";
     link.download = "Abhishri_Medewar_Resume.pdf"; // Specify the file name you want the user to download as
     link.target = "_blank"; // Open the file in a new tab
 
